4474f67f2719600e69688b99cf6d29bb6ba31840
yenoh <tessemayenoh94@gmail.com>
2024-07-30 04:08:37 +0300

    Top bar modified

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,13 +1,10 @@
-"use client";
-
 import { SignedIn, UserButton } from "@clerk/nextjs";
-import { AlignJustify } from "lucide-react";
 import React from "react";
 import NavDrawer from "./NavDrawer";
 
 function TopBar() {
   return (
-    <header className="bg-[#1c1f2e] flex  px-5 py-3  w-full justify-between md:justify-end">
+    <header className="bg-[#1c1f2e] flex  px-5 py-3  w-full justify-between sticky top-0 md:justify-end">
       <div className="block md:hidden">
         <NavDrawer side={"left"} />
       </div>
