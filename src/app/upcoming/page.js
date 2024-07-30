dbd13875d4766c8dc9a99b46430979b107b260d0
yenoh <tessemayenoh94@gmail.com>
2024-07-30 04:07:50 +0300

    Upcoming page implemented

--- src/app/upcoming/page.js
+++ src/app/upcoming/page.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import { FutureMeeting } from "@/components/FutureMeetings";
+const futureMeetings = [
+  {
+    title: "Team Sync: Sprint Planning & Updates",
+    date: "March 15, 2024 - 10:00 AM",
+  },
+  {
+    title: "Project Pulse Check: Weekly Standup ",
+    date: "March 15, 2024 - 10:00 AM",
+  },
+];
 function page() {
-  return <div>upcoming</div>;
+  return (
+    <main>
+      <h1 className="font-bold text-xl mb-4">Upcoming Meetings</h1>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {futureMeetings.map(({ title, date }, index) => {
+          return (
+            <FutureMeeting
+              key={index}
+              title={title}
+              date={date}
+              previous={false}
+            />
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default page;
