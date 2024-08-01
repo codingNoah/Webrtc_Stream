0a7c75cadbac4c62cb82e001872ceb9678c41d37
yenoh <tessemayenoh94@gmail.com>
2024-08-01 11:24:58 +0300

    Call Type url path changed

--- src/app/meeting/[id]/page.js
+++ src/app/meeting/[id]/page.js
@@ -3,24 +3,18 @@ import MeetingUI from "@/components/MeetingUI";
 import {
   StreamTheme,
   StreamCall,
-  PaginatedGridLayout,
-  CallControls,
-  useCall,
   useStreamVideoClient,
-  useCallStateHooks,
 } from "@stream-io/video-react-sdk";
 import { Loader2 } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import { OwnCapability } from "@stream-io/video-react-sdk";
 
-function Meeting({ params, searchParams }) {
+function Meeting({ params }) {
   const [callNotFound, setCallNotFound] = useState(false);
 
   const [call, setCall] = useState();
 
   const callID = params.id;
   const client = useStreamVideoClient();
-  const { useHasPermissions } = useCallStateHooks();
 
   useEffect(() => {
     const loadCall = async () => {
@@ -36,20 +30,7 @@ function Meeting({ params, searchParams }) {
           return;
         }
 
-        console.log(calls[0].type, callID);
-        const currentCall = client.call(searchParams.type, callID);
-
-        const canJoin = currentCall.permissionsContext.hasPermission(
-          OwnCapability.JOIN_CALL
-        );
-
-        console.log("canJoin", canJoin);
-
-        // console.log(canJoin);
-
-        // if (!canJoin) {
-        //   throw new Error("");
-        // }
+        const currentCall = calls[0];
 
         setCall(currentCall);
       } catch (error) {
@@ -60,7 +41,7 @@ function Meeting({ params, searchParams }) {
     };
 
     loadCall();
-  }, [callID, client, searchParams.type]);
+  }, [callID, client]);
 
   if (callNotFound) {
     return (
